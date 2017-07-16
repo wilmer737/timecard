d84a070a677d6ab0a4cfe568a44dc3d696dfab19
Wilmer <pollos737@outlook.com>
2017-07-16 12:30:07 -0700

    installed react router and created template home page

--- app/components/app.js
+++ app/components/app.js
@@ -1,42 +1,31 @@
 import React from 'react'
 import {
-  Button,
-  Container,
-  Segment,
-  Form
-} from 'semantic-ui-react'
+  BrowserRouter,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import Home from './home'
+import NewTimeForm from './log-new-time'
 
 class App extends React.Component {
   constructor() {
     super()
-
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleSubmit(e) {
-    e.preventDefault()
-    console.log('sup')
   }
 
   render() {
     return(
-      <Container text>
-        <Segment raised padded='very'>
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Field>
-              <label>Start Time</label>
-              <input type="date" className="start-date" defaultValue='2017-06-23' />
-              <input type="time" className="start-time" />
-            </Form.Field>
-            <Form.Field>
-              <label>End Time</label>
-              <input type="date" className="end-date" defaultValue='2017-06-23' />
-              <input type="time" className="end-time" />
-            </Form.Field>
-            <Button color="teal" floated="right">Submit</Button>
-          </Form>
-        </Segment>
-      </Container>
+      <BrowserRouter>
+        <div>
+          <Link to="/">Home Page</Link>
+          <Link to="/log-time">New Time</Link>
+
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/log-time" component={NewTimeForm} />
+        </div>
+      </BrowserRouter>
     )
   }
 }
---
+++ app/components/home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => (
+  <div>
+    This is the sparta
+  </div>
+)
+
+export default Home
